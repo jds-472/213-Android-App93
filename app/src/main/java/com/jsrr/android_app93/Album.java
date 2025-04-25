034d5a0cebfd264d39955e490baea25d158a7914
jds-472 <jds472@scarletmail.rutgers.edu>
2025-04-25 17:33:54 -0400

    The extra files

---
+++ app/src/main/java/com/jsrr/android_app93/Album.java
@@ -0,0 +1,112 @@
+package com.jsrr.android_app93;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+/**
+ * The {@code Album} class represents a photo album that contains a collection of photos.
+ * It provides functionality to manage photos, retrieve album details, and determine
+ * the earliest and latest dates of the photos in the album.
+ *
+ * <p>This class implements {@link Serializable} to allow serialization of album objects.
+ *
+ * <p>Features of the {@code Album} class include:
+ * <ul>
+ *   <li>Adding and removing photos</li>
+ *   <li>Retrieving the total number of photos</li>
+ *   <li>Getting the earliest and latest dates of photos</li>
+ * </ul>
+ *
+ * @author [Joseph Scarpulla and Roger Ramirez]
+ * @version 1.0
+ */
+public class Album implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private Set<Photo> photos = new HashSet<>();
+
+    /**
+     * Constructs an {@code Album} with the specified name.
+     *
+     * @param name the name of the album
+     */
+    public Album(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Constructs an {@code Album} with the specified name and a set of photos.
+     *
+     * @param name   the name of the album
+     * @param photos the set of photos to initialize the album with
+     */
+    public Album(String name, Set<Photo> photos) {
+        this.name = name;
+        this.photos = photos;
+    }
+
+    /**
+     * Retrieves the name of the album.
+     *
+     * @return the name of the album
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the album.
+     *
+     * @param name the new name of the album
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Retrieves the set of photos in the album.
+     *
+     * @return the set of photos in the album
+     */
+    public Set<Photo> getPhotos() {
+        return photos;
+    }
+
+    /**
+     * Returns the string representation of the album, which is its name.
+     *
+     * @return the name of the album
+     */
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    /**
+     * Retrieves the total number of photos in the album.
+     *
+     * @return the total number of photos in the album
+     */
+    public int getPhotoCount() {
+        return photos.size();
+    }
+
+    /**
+     * Adds a photo to the album.
+     *
+     * @param photo the photo to be added to the album
+     */
+    public void addPhoto(Photo photo) {
+        photos.add(photo);
+    }
+
+    /**
+     * Removes a photo from the album.
+     *
+     * @param photo the photo to be removed from the album
+     */
+    public void removePhoto(Photo photo) {
+        photos.remove(photo);
+    }
+}
---
+++ app/src/main/java/com/jsrr/android_app93/Tag.java
@@ -0,0 +1,92 @@
+package com.jsrr.android_app93;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The {@code Tag} class represents a tag that is part of a Photo.
+ * It provides functionality to manage the name and value of the tag.
+ * It also provides methods to compare tags and generate a string representation of the tag.
+ *
+ * <p>This class implements {@link Serializable} to allow serialization of album objects.
+ *
+ * <p>Features of the {@code Tag} class include:
+ * <ul>
+ *   <li>Managing instance fields</li>
+ *   <li>Retrieving a string representation of tag</li>
+ *   <li>Comparing equality and getting a hashcode</li>
+ * </ul>
+ *
+ * @author [Joseph Scarpulla and Roger Ramirez]
+ * @version 1.0
+ */
+public class Tag implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private String value;
+
+    public static List<String> tagTypes = new ArrayList<>();
+    static {
+        tagTypes.add("Location");
+        tagTypes.add("Person");
+    }
+
+    /**
+     * Constructs a {@code Tag} with the specified name and value.
+     * @param name the name of the tag
+     * @param value the value of the tag
+     */
+    public Tag(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    /**
+     * Retrieves the name of the tag.
+     * @return the name of the tag
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Retrieves the value of the tag.
+     * @return the value of the tag
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Retrieves the Tag's String representation.
+     * @return the string representation of the tag
+     */
+    @Override
+    public String toString() {
+        return name + ": " + value;
+    }
+
+    /**
+     * Compares this tag to the specified object for equality.
+     * @param obj the object to compare
+     * @return true if the specified object is equal to this tag, false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Tag other = (Tag) obj;
+        return name.equalsIgnoreCase(other.name) && value.equalsIgnoreCase(other.value);
+    }
+
+    /**
+     * Returns the hash code value for this tag.
+     * @return the hash code value for this tag
+     */
+    @Override
+    public int hashCode() {
+        return (name.toLowerCase() + ":" + value.toLowerCase()).hashCode();
+    }
+}
